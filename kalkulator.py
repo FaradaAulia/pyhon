b119459581d9f0bfc1c9c4783153561ca6d4a32e
Farada Aulia <116609196+FaradaAulia@users.noreply.github.com>
2024-09-30 08:31:47 +0700

    Create kalkulator.py

---
+++ kalkulator.py
@@ -0,0 +1,32 @@
+print("Masukkan Pilihan Anda")
+x = int(input())
+
+if x == 1:
+    print("Ini adalah pertambahan")
+    print("Masukkan Bilangan Pertama")
+    bilangan1 = int(input())
+    print("Masukkan Bilangan Kedua")
+    bilangan2 = int(input())
+    hasil = bilangan1 + bilangan2
+    print("Hasilnya adalah", hasil)
+
+elif x == 2:
+    print("Ini adalah Pengurangan")
+    print("Masukkan Bilangan Pertama")
+    bilangan1 = int(input())
+    print("Masukkan Bilangan Kedua")
+    bilangan2 = int(input())
+    hasil = bilangan1 - bilangan2
+    print("Hasilnya adalah", hasil)
+
+elif x == 3:
+    print("Ini adalah penentu bilangan ganjil atau genap")
+    print("Masukkan Angka Anda")
+    angka = int(input())
+    if angka % 2 == 0:
+        print(angka,"adalah Genap")
+    else:
+        print(angka, "adalah Ganjil")
+
+else:
+    print("Fitur selanjutnya segera datang")

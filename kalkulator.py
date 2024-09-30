928114ebde8428f663d4fec34b1b2487db41452d
Farada Aulia <116609196+FaradaAulia@users.noreply.github.com>
2024-09-30 08:38:45 +0700

    Update kalkulator.py
    while

--- kalkulator.py
+++ kalkulator.py
@@ -1,32 +1,38 @@
-print("Masukkan Pilihan Anda")
-x = int(input())
 
-if x == 1:
-    print("Ini adalah pertambahan")
-    print("Masukkan Bilangan Pertama")
-    bilangan1 = int(input())
-    print("Masukkan Bilangan Kedua")
-    bilangan2 = int(input())
-    hasil = bilangan1 + bilangan2
-    print("Hasilnya adalah", hasil)
+while True:
+    print("Masukkan Pilihan Anda")
+    x = int(input())
 
-elif x == 2:
-    print("Ini adalah Pengurangan")
-    print("Masukkan Bilangan Pertama")
-    bilangan1 = int(input())
-    print("Masukkan Bilangan Kedua")
-    bilangan2 = int(input())
-    hasil = bilangan1 - bilangan2
-    print("Hasilnya adalah", hasil)
+    if x == 1:
+        print("Ini adalah pertambahan")
+        print("Masukkan Bilangan Pertama")
+        bilangan1 = int(input())
+        print("Masukkan Bilangan Kedua")
+        bilangan2 = int(input())
+        hasil = bilangan1 + bilangan2
+        print("Hasilnya adalah", hasil)
 
-elif x == 3:
-    print("Ini adalah penentu bilangan ganjil atau genap")
-    print("Masukkan Angka Anda")
-    angka = int(input())
-    if angka % 2 == 0:
-        print(angka,"adalah Genap")
+    elif x == 2:
+        print("Ini adalah Pengurangan")
+        print("Masukkan Bilangan Pertama")
+        bilangan1 = int(input())
+        print("Masukkan Bilangan Kedua")
+        bilangan2 = int(input())
+        hasil = bilangan1 - bilangan2
+        print("Hasilnya adalah", hasil)
+
+    elif x == 3:
+        print("Ini adalah penentu bilangan ganjil atau genap")
+        print("Masukkan Angka Anda")
+        angka = int(input())
+        if angka % 2 == 0:
+            print(angka,"adalah Genap")
+        else:
+            print(angka, "adalah Ganjil")
+
     else:
-        print(angka, "adalah Ganjil")
+        print("Fitur selanjutnya segera datang")
 
-else:
-    print("Fitur selanjutnya segera datang")
+    ulang = input("Ingin Mengulang? (y/n) :")
+    if ulang.lower() != 'y':
+        break
